45d3e53bec2a99dbd76d1d86ac74434033c63308
jaideesukanya <s.jaidee@togetherteam.co.th>
2017-10-05 11:24:07 +0700

    Commit js lib

---
+++ App/Scripts/app.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+angular.module('catApp', ['ngRoute','AdalAngular'])
+.config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
+
+    $routeProvider.when("/Home", {
+        controller: "homeCtrl",
+        templateUrl: "/App/Views/Home.html",
+    }).when("/CatList", {
+        controller: "catListCtrl",
+        templateUrl: "/App/Views/CatList.html",
+        requireADLogin: true,
+    }).when("/UserData", {
+        controller: "userDataCtrl",
+        templateUrl: "/App/Views/UserData.html",
+    }).otherwise({ redirectTo: "/Home" });
+
+    adalProvider.init(
+        {
+            instance: 'https://login.microsoftonline.com/', 
+            tenant: 'TOGETHER.AT',
+            clientId: '84a30349-7c6d-42fc-a92e-fa7b774f66e9',
+			//clientSecret: '58DvDFnJQMQK/K2w1KuqnjVGws1HtE4SfP8NnJX0iWc=',
+            extraQueryParameter: 'nux=1',
+            //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+        },
+        $httpProvider
+        );
+   
+}]);
